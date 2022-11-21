f0f64ad73b857ec8393c1e783df2cd70b7284ad0
sjaehn <kontakt@jahnichen.de>
2022-11-21 18:42:50 +0100

    Use new setZoom()

--- BAmp_GUI.cpp
+++ BAmp_GUI.cpp
@@ -41,11 +41,13 @@ void BAmp_GUI::portEvent (uint32_t port_index, uint32_t buffer_size, uint32_t fo
 
 void BAmp_GUI::onConfigureRequest (BEvents::Event* event)
 {
-	Window::onConfigureRequest (event);
+	BEvents::ExposeEvent* ev = dynamic_cast<BEvents::ExposeEvent*>(event);
+    if (!ev) return;
+    Window* w = dynamic_cast<Window*>(ev->getWidget());
+    if (!w) return;
 
-	double sz = (getWidth() > getHeight() ? getHeight() : getWidth()) / 100;
-	dial.moveTo (10 * sz, 10 * sz);
-	dial.resize (80 * sz, 80 * sz);
+	double sz = (ev->getArea().getWidth() > ev->getArea().getHeight() ? ev->getArea().getHeight() : ev->getArea().getWidth()) / 100;
+	setZoom (sz);
 }
 
 void BAmp_GUI::valueChangedCallback (BEvents::Event* event)
